d87758e5c21417ecd919e8778a1d2c5f329fbf46
Nisho Matsushita <nichyou@gmail.com>
2017-07-01 04:12:13 +0900

    論文とその印刷物の追加。mseと周回時の結果を追加

--- imagingSystem/imagingSystem/main.cpp
+++ imagingSystem/imagingSystem/main.cpp
@@ -6,6 +6,48 @@
 #include <opencv2/xfeatures2d.hpp>
 #include <vector>
 #include <string>
+#include <iostream>
+#include <direct.h>
+#include <sstream>
+#include <fstream>
+
+double mean_squeare_error(cv::Mat origin, cv::Mat edited, int iterate_num, std::string out_dir) {
+	cv::Mat gray_o, gray_e, gray16s_o, gray16s_e;
+	cv::cvtColor(origin, gray_o, CV_BGR2GRAY);
+	cv::cvtColor(edited, gray_e, CV_BGR2GRAY);
+	gray_o.convertTo(gray16s_o, CV_16S);
+	gray_e.convertTo(gray16s_e, CV_16S);
+
+	gray16s_o -= gray16s_e;
+
+	cv::Scalar s = cv::sum(gray16s_o.mul(gray16s_o));
+
+	cv::Mat gray8u_o;
+	gray16s_o.convertTo(gray8u_o, CV_8U);
+	imshow("result img4", gray8u_o);
+	cv::imwrite(out_dir + "\\diff" + std::to_string(iterate_num+1) + ".jpg", gray8u_o);
+	//cv::waitKey(0);
+
+	edited = gray16s_o;
+
+	return s[0] / gray16s_o.rows / gray16s_o.cols;
+}
+
+std::vector<std::string> split(const std::string &str, char sep)
+{
+	std::vector<std::string> v;
+	auto first = str.begin(); 
+	while (first != str.end()) {        
+		auto last = first;               
+		while (last != str.end() && *last != sep)       
+			++last;
+		v.push_back(std::string(first, last));   
+		if (last != str.end())
+			++last;
+		first = last;
+	}
+	return v;
+}
 
 void main() {
 	using cv::imread;
@@ -20,77 +62,96 @@ void main() {
 	Mat gray[2];
 	Mat result;
 
-	src[0] = imread("lena.jpg");
-	src[1] = imread("printed_lena.jpg");
+	std::string origin_name = "653-origin.jpg";
+	std::string photo_name = "653-top-notes.jpg";
+
+
+	// 結果保存用のフォルダとファイル
+	char out_dir[] = "";
+	std::string out_file = "output.txt";
+	std::ofstream writing_file;
+
+	time_t now = time(NULL);
+	struct tm *pnow = localtime(&now);
+	sprintf(out_dir, "results\\%s%02d%02d%02d%02d", 
+		split(photo_name, '.')[0].c_str(), pnow->tm_mon + 1, pnow->tm_mday, pnow->tm_hour, pnow->tm_min);
+	_mkdir(out_dir);
+	writing_file.open(std::string(out_dir)+"\\"+out_file, std::ios::out);
+
+	src[1] = imread(origin_name);
+	src[0] = imread(photo_name);
+
+	cv::resize(src[0], src[0], cv::Size(), 0.5, 0.5);
+	cv::resize(src[1], src[1], cv::Size(), 0.5, 0.5);
 	
-	cv::cvtColor(src[0], gray[0], CV_BGR2GRAY);
-	cv::cvtColor(src[1], gray[1], CV_BGR2GRAY);
+	//src[0] = imread("lena.jpg");
+	//src[1] = imread("printed_lena.jpg");
 
-	// gray 画像にする
-	//gray[0].copyTo(src[0]);
+	int iter_num = 5;
 
-	//cv::Ptr<cv::xfeatures2d::SIFT> detector = cv::xfeatures2d::SIFT::create();
-	cv::Ptr<cv::xfeatures2d::SURF> detector = cv::xfeatures2d::SURF::create();
+	for (int j = 0; j < iter_num; j++) {
+		cv::cvtColor(src[0], gray[0], CV_BGR2GRAY);
+		cv::cvtColor(src[1], gray[1], CV_BGR2GRAY);
 
-	vector<cv::KeyPoint> keypoints[2];
-	Mat descriptors[2];
-	for (int i = 0; i < 2; i++) {
-		detector->detect(gray[i], keypoints[i]);
-		detector->compute(gray[i], keypoints[i], descriptors[i]);
-	}
+		// gray 画像にする
+		//gray[0].copyTo(src[0]);
 
-	vector<cv::DMatch> matches;
+		//cv::Ptr<cv::xfeatures2d::SIFT> detector = cv::xfeatures2d::SIFT::create();
+		cv::Ptr<cv::xfeatures2d::SURF> detector = cv::xfeatures2d::SURF::create();
 
-	cv::BFMatcher matcher;
-	matcher.match(descriptors[0], descriptors[1], matches);
-
-	vector<cv::Vec2f> points1(matches.size());
-	vector<cv::Vec2f> points2(matches.size());
-
-	for (size_t i = 0; i < matches.size(); ++i)
-	{
-		points1[i][0] = keypoints[0][matches[i].queryIdx].pt.x;
-		points1[i][1] = keypoints[0][matches[i].queryIdx].pt.y;
-
-		points2[i][0] = keypoints[1][matches[i].trainIdx].pt.x;
-		points2[i][1] = keypoints[1][matches[i].trainIdx].pt.y;
-	}
-
-	Mat matchedImg;
-	drawMatches(src[0], keypoints[0], src[1], keypoints[1], matches, matchedImg);
-	imshow("draw img", matchedImg);
-	waitKey();
-
-
-
-	Mat homo = cv::findHomography(points1, points2, CV_RANSAC);
-	cv::warpPerspective(src[0], result, homo, Size(static_cast<int>(src[1].cols), static_cast<int>(src[1].rows)));
-	waitKey();
-	for (int y = 0; y < src[0].rows; y++) {
-		for (int x = 0; x < src[0].cols; x++) {
-			result.at<Vec3b>(y, x) = src[1].at<Vec3b>(y, x);
+		vector<cv::KeyPoint> keypoints[2];
+		Mat descriptors[2];
+		for (int i = 0; i < 2; i++) {
+			detector->detect(gray[i], keypoints[i]);
+			detector->compute(gray[i], keypoints[i], descriptors[i]);
 		}
-	}
 
-	imshow("result img", result);
-	waitKey(0);
+		vector<cv::DMatch> matches;
 
-	Mat result2;
+		cv::BFMatcher matcher;
+		matcher.match(descriptors[0], descriptors[1], matches);
 
-	Mat homo2 = cv::findHomography(points2, points1, CV_RANSAC);
-	cv::warpPerspective(result, result2, homo2, Size(static_cast<int>(src[1].cols), static_cast<int>(src[1].rows)));
+		vector<cv::Vec2f> points1(matches.size());
+		vector<cv::Vec2f> points2(matches.size());
 
-	imshow("warped result", result2);
-	waitKey(0);
+		for (size_t i = 0; i < matches.size(); ++i)
+		{
+			points1[i][0] = keypoints[0][matches[i].queryIdx].pt.x;
+			points1[i][1] = keypoints[0][matches[i].queryIdx].pt.y;
 
-	for (int y = 0; y < src[0].rows; y++) {
-		for (int x = 0; x < src[0].cols; x++) {
-			result2.at<Vec3b>(y, x) -= src[0].at<Vec3b>(y, x);
+			points2[i][0] = keypoints[1][matches[i].trainIdx].pt.x;
+			points2[i][1] = keypoints[1][matches[i].trainIdx].pt.y;
 		}
+
+		Mat matchedImg;
+		drawMatches(src[0], keypoints[0], src[1], keypoints[1], matches, matchedImg);
+		imshow("draw img", matchedImg);
+		//waitKey(0);
+
+		Mat homo = cv::findHomography(points1, points2, CV_RANSAC);
+		cv::warpPerspective(src[0], result, homo, Size(static_cast<int>(src[1].cols), static_cast<int>(src[1].rows)));
+
+		imshow("result img", result);
+		cv::imwrite(std::string(out_dir) + "\\result" + std::to_string(j+1) + ".jpg", result);
+		//waitKey(0);
+
+		/*for (int y = 0; y < src[1].rows; y++) {
+		for (int x = 0; x < src[1].cols; x++) {
+		cv::absdiff(result.at<Vec3b>(y, x), src[1].at<Vec3b>(y, x), result.at<Vec3b>(y, x));
+		}
+		}
+
+		cv::Mat crop = result(cv::Rect(0, 0, src[1].cols, src[1].rows));
+
+		imshow("substracttion", crop);
+		waitKey(0);*/
+
+		double mse = mean_squeare_error(src[1], result, j, std::string(out_dir));
+
+		std::cout << mse << std::endl;
+		writing_file << mse << std::endl;
+
+		src[0] = result;
+
 	}
-
-	cv::Mat crop = result2(cv::Rect(0, 0, src[0].cols, src[0].rows));
-
-	imshow("substracttion", crop);
-	waitKey(0);
 }
